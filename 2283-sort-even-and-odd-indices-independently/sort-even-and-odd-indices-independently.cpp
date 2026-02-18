50fa76b70936263ca1c1102d54b9958f5bf5a074
LeelaSravanthi18 <22P31A04E5@acet.ac.in>
2026-02-18 08:45:58 +0530

    Time: 6 ms (1.91%) | Memory: 16.1 MB (17.21%) - LeetSync

---
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>even,odd;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                even.push_back(nums[i]);
+            }
+            else
+            {
+                odd.push_back(nums[i]);
+            }
+        }
+        sort(even.begin(),even.end());
+        sort(odd.begin(),odd.end(),greater<int>());
+        int e = 0, o = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(i % 2 == 0)
+            {
+                nums[i] = even[e];
+                e=e+1;
+            }
+            else
+            {
+                nums[i] = odd[o];
+                o=o+1;
+            }
+        }
+        return nums;
+    }
+};
